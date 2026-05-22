f739987d5ea1f4ccad50be5c66f88d49ebb4f47d
Cvecc <claravecchios@gmail.com>
2026-05-22 10:46:52 -0300

    feat: Layout padronizado, cores, fundos, strings

--- app/src/main/java/com/example/petconnect/MainActivity.java
+++ app/src/main/java/com/example/petconnect/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.petconnect;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_tela_login);
-
-
 
     }
 }
--- app/src/main/java/com/example/petconnect/TelaCadastro.java
+++ app/src/main/java/com/example/petconnect/TelaCadastro.java
@@ -1,6 +1,8 @@
 package com.example.petconnect;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaCadastro extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
+public class TelaCadastro extends AppCompatActivity {
+    Spinner spEstado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+        spEstado = findViewById(R.id.spEstado);
 
+        List<String> estados = Arrays.asList(
+                "Selecione um estado",
+                "São Paulo",
+                "Rio de Janeiro",
+                "Minas Gerais"
+        );
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.spinner_item,
+                estados
+        );
+
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+
+        spEstado.setAdapter(adapter);
 
     }
 
