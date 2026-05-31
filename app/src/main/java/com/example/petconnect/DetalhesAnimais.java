69f607ffad06f21e05b66fda8505d0d4fdd5926c
barbarabrandino <bahbrandino@gmail.com>
2026-05-31 15:52:52 -0300

    feat: adcionado tela de detalhes e novo layout do dashboard da ong

---
+++ app/src/main/java/com/example/petconnect/DetalhesAnimais.java
@@ -0,0 +1,20 @@
+package com.example.petconnect;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetalhesAnimais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detalhes_animais);
+
+    }
+}
